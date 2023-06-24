63226f7eefc4253047b13bd0587fb5e585c72b59
Ishi <syi378039@gmail.com>
2023-06-24 16:58:15 +0900

    a

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 14:01:43 by tsishika          #+#    #+#             */
-/*   Updated: 2023/06/21 16:41:58 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/06/24 10:31:51 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,13 @@ int	ft_judge_push(t_arg *arg, char type, int print)
 
 	if (type == 'a')
 	{
-		result = pa(arg);
+		result = ft_push_a(arg);
 		if (result != 0 && print != 0)
 			ft_putstr_fd("pa\n", 1);
 	}
 	if (type == 'b')
 	{
-		result = pb(arg);
+		result = ft_push_b(arg);
 		if (result != 0 && print != 0)
 			ft_putstr_fd("pb\n", 1);
 	}
--- swap.c
+++ swap.c
@@ -6,7 +6,61 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 14:01:53 by tsishika          #+#    #+#             */
-/*   Updated: 2023/06/20 21:08:59 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/06/24 16:57:53 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+static int	ft_swap_a(t_arg *arg)
+{
+	t_node	*node1;
+	t_node	*node2;
+
+	if (arg->a->size < 2)
+		return (0);
+	node1 = ft_pop(arg->a);
+	node2 = ft_pop(arg->a);
+	ft_push(arg->a, node1);
+	ft_push(arg->a, node2);
+	return (1);
+}
+
+static int	ft_swap_b(t_arg *arg)
+{
+	t_node	*node1;
+	t_node	*node2;
+
+	if (arg->b->size < 2)
+		return (0);
+	node1 = ft_pop(arg->b);
+	node2 = ft_pop(arg->b);
+	ft_push(arg->b, node1);
+	ft_push(arg->b, node2);
+	return (1);
+}
+
+int	ft_judge_swap(t_arg *arg, char type, int print)
+{
+	int result;
+
+	if (type == 'a')
+	{
+		result = ft_swap_a(arg);
+		if (result != 0 && print != 0)
+			ft_putstr_fd("sa\n", 1);
+	}
+	if (type == 'b')
+	{
+		result = ft_swap_b(arg);
+		if (result != 0 && print != 0)
+			ft_putstr_fd("sb\n", 1);
+	}
+	if (type == 's')
+	{
+		result = ft_swap_a(arg) + ft_swap_b(arg);
+		if (result == 2 && print != 0)
+			ft_putstr_fd("ss\n", 1);
+	}
+	return (0);
+}
