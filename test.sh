b15a3eff78e5698269eab6917696356162781898
Ishi <syi378039@gmail.com>
2023-07-24 11:44:37 +0900

    add tester

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ echo "[Tests using nafuka11 tester]"
 cd push_swap_tester
 echo -e "\033[31m\033[47m********************引数3のとき********************\033[m\033[m"
 python3 push_swap_tester.py -l 3
-echo -e "\033[31m\033[47m********************引数12のとき********************\033[m\033[m"
+echo -e "\033[31m\033[47m********************引数5のとき********************\033[m\033[m"
 python3 push_swap_tester.py -l 5
 echo -e "\033[31m\033[47m********************引数100のとき********************\033[m\033[m"
 python3 push_swap_tester.py -l 100
